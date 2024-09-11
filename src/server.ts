85d85f96a92b1ce534b7e648043f74d133d98b35
MaxvG115 <maxivillegas115@gmail.com>
2024-09-10 20:48:10 -0600

    Created router | no responses

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import router from './router'
 
 const app = express()
 
@@ -6,5 +7,7 @@ app.get('/', (req, res) => {
     console.log('Hello from express')
     res.status(200).json({ message: "Hello" })
 })
+
+app.use('/api', router)
 
 export default app
