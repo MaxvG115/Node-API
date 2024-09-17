e9370ae443e9238eae0c273c7a34d04fcd9a4694
MaxvG115 <maxivillegas115@gmail.com>
2024-09-16 18:20:27 -0600

    Product handlers

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express'
+import { body, check, oneOf } from 'express-validator'
+import { handleInputErrors } from './modules/middleware'
+import { createProduct, getOneProduct, getProducts } from './handlers/product'
 
 const router = Router()
 
@@ -6,12 +9,12 @@ const router = Router()
  * Product
 */
 
-router.get('/product', (req,res) => {
-    res.json({ message: 'message' })
-} )
+router.get('/product', getProducts)
 router.get('/product/:id', () => {} )
-router.put('/product/:id', () => {} )
-router.post('/product', () => {} )
+router.put('/product/:id',body('name').isString(), handleInputErrors , (req,res) => {
+    
+})
+router.post('/product',body('name').isString(), handleInputErrors , createProduct )
 router.delete('/product/:id', () => {} )
 
 /*
@@ -20,8 +23,18 @@ router.delete('/product/:id', () => {} )
 
 router.get('/update', () => {} )
 router.get('/update/:id', () => {} )
-router.put('/update/:id', () => {} )
-router.post('/update', () => {} )
+router.put('/update/:id',
+    body('title').isString().optional,
+    body('body').isString().optional, 
+    oneOf([check('status').equals('IN_PROGRESS'), check('status').equals('SHIPPED'), check('status').equals('DEPRECATED')]), 
+    body('version').isString().optional,  
+    handleInputErrors , 
+    () => {})
+router.post('/update', 
+    body('title').exists().isString(),
+    body('body').exists().isString(), 
+    handleInputErrors , 
+    () => {})
 router.delete('/update/:id', () => {} )
 
 /*
@@ -30,8 +43,8 @@ router.delete('/update/:id', () => {} )
 
 router.get('/updatepoint', () => {} )
 router.get('/updatepoint/:id', () => {} )
-router.put('/updatepoint/:id', () => {} )
-router.post('/updatepoint', () => {} )
+router.put('/updatepoint/:id', body('name').optional().isString(), body('description').optional().isString() ,() => {} )
+router.post('/updatepoint', body('name').exists().isString(), body('description').exists().isString(), body('updateId').exists().isString() ,() => {} )
 router.delete('/updatepoint/:id', () => {} )
 
 export default router
